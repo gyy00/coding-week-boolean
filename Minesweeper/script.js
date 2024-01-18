97b6cbb89bb29a3aae31b001217862822bc5efe5
gyy00 <giyiany@gmail.com>
2024-01-18 14:08:02 +0000

    Add files via upload

---
+++ Minesweeper/script.js
@@ -0,0 +1,92 @@
+const scoreCounter = document.querySelector('.score-counter');
+const grid = document.querySelector('.grid');
+const endGameScreen = document.querySelector('.end-game-screen');
+const endGameText = document.querySelector('.end-game-text');
+const playAgainButton = document.querySelector('.play-again');
+
+const totalCells = 100;
+const totalBombs = 8;
+const maxScore = totalCells - totalBombs;
+const bombsList = [];
+
+let score = 0;
+
+//generate random bomb locations
+while (bombsList.length < totalBombs){
+    const randomNumber = Math.floor(Math.random() * totalCells) +1;
+
+    if (!bombsList.includes(randomNumber)) {
+        bombsList.push(randomNumber);
+    }
+}
+
+
+
+for (let i = 1; i <= totalCells; i++) {
+    // cell = <div></div>
+    const cell = document.createElement('div');
+    // cell = <div class="cell"></div>
+    cell.classList.add('cell');
+
+    //cell.innerText = i;
+  
+    //execute function when clicked
+    cell.addEventListener('click', function () {
+        // Don't do anything if it is already clicked
+        if (cell.classList.contains('cell-clicked')) {
+          return;
+        }
+    
+        if (bombsList.includes(i)) {
+          cell.classList.add('cell-bomb');
+          endGame(false);
+        } else {
+          cell.classList.add('cell-clicked');
+          updateScore();
+        }
+    });
+  
+    grid.appendChild(cell);
+  }
+
+
+
+
+//updates the score counter
+function updateScore(){
+    score++;
+    scoreCounter.innerText = score.toString().padStart(5, '0');
+
+    if (score == maxScore){
+        endGame(true);
+    }
+}
+
+function endGame(isVictory) {
+  if (isVictory) {
+    endGameText.innerHTML = 'YOU<br>WON';
+    endGameScreen.classList.add('win');
+  }
+
+  revealAllBombs();
+  endGameScreen.classList.remove('hidden');
+}
+
+
+function revealAllBombs() {
+    const cells = document.querySelectorAll('.cell');
+  
+    for (let i = 1; i <= cells.length; i++) {
+      const cell = cells[i - 1];
+  
+      if (bombsList.includes(i)) {
+        cell.classList.add('cell-bomb');
+      }
+    }
+  }
+  
+  playAgainButton.addEventListener('click', function () {
+    window.location.reload();
+  });
+
+
